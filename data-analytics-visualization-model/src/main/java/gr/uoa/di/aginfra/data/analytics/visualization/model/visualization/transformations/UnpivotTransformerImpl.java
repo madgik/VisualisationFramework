9a7985d3eb3f7cccb6b305dd364d1f75ab5cfcc9
cmitatakis <cmitatakis@gmail.com>
2018-09-26 11:56:50 +0300

    CSV unpivot class deleted

--- data-analytics-visualization-model/src/main/java/gr/uoa/di/aginfra/data/analytics/visualization/model/visualization/transformations/UnpivotTransformerImpl.java
+++ data-analytics-visualization-model/src/main/java/gr/uoa/di/aginfra/data/analytics/visualization/model/visualization/transformations/UnpivotTransformerImpl.java
@@ -1,8 +1,12 @@
 package gr.uoa.di.aginfra.data.analytics.visualization.model.visualization.transformations;
 
 import gr.uoa.di.aginfra.data.analytics.visualization.model.definitions.Transformation;
-import gr.uoa.di.aginfra.data.analytics.visualization.model.helpers.CSVUnpivot;
+import gr.uoa.di.aginfra.data.analytics.visualization.model.definitions.UnpivotStructure;
+import gr.uoa.di.aginfra.data.analytics.visualization.model.exceptions.InvalidFormatException;
 import gr.uoa.di.aginfra.data.analytics.visualization.model.visualization.data.DataSet;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UnpivotTransformerImpl implements UnpivotTransformer {
 
@@ -10,8 +14,73 @@ public class UnpivotTransformerImpl implements UnpivotTransformer {
     public DataSet unPivot(DataSet dataSet, Transformation transformation) throws Exception {
 
         if(transformation != null) {
-            DataSet unpivot = CSVUnpivot.unpivotCSV(dataSet, transformation);
-            return unpivot;
+
+            UnpivotStructure unpivotStructure = new UnpivotStructure();
+
+            try {
+                List<String> updatedFields = new ArrayList<>();
+                List<Integer> columnsPosition = new ArrayList<>();
+
+                for (int j = 0; j < dataSet.getFields().size(); j++) {
+                    String f = dataSet.getFields().get(j);
+                    boolean addColumn = true;
+                    for(String column : transformation.getTransformationColumns()){
+                        if(f.equals(column))
+                            addColumn = false;
+                    }
+                    if(addColumn) {
+                        updatedFields.add(f);
+                    }
+                    else
+                        columnsPosition.add(j);
+
+                }
+                updatedFields.add(transformation.getTransformationLabel());
+                updatedFields.add(transformation.getTransformationLabelValue());
+                unpivotStructure.setNewColumnValue(transformation.getTransformationLabelValue());
+                unpivotStructure.setNewColumnName(transformation.getTransformationLabel());
+                unpivotStructure.setColumnsToUnpivot(transformation.getTransformationColumns());
+                unpivotStructure.setColumnsPosition(columnsPosition);
+
+                List<List<String>> list = new ArrayList<>();
+
+                for (int i = 1; i < dataSet.getData().size(); i++) {
+                    for (Integer oldColumns : unpivotStructure.getColumnsPosition()) {
+                        List<String> item = new ArrayList<>();
+                        boolean oldColumnFound = false;
+                        int oldColumnPosition = 0;
+                        String oldColumnValue = null;
+                        for (int j = 0; j < dataSet.getFields().size(); j++) {
+                            String f = dataSet.getFields().get(j);
+                            if (dataSet.getData().get(i).size() > j ) {
+                                if(!oldColumnFound && oldColumns == j)
+                                {
+                                    oldColumnValue = dataSet.getData().get(i).get(j);
+                                    oldColumnFound = true;
+                                    oldColumnPosition = j;
+                                }
+                                else if(oldColumns != j && !unpivotStructure.getColumnsPosition().contains(j))
+                                    item.add(dataSet.getData().get(i).get(j));
+                            } else item.add( null);
+                        }
+                        String[] s = dataSet.getFields().get(oldColumnPosition).split("-");
+                        if(s.length > 1) {
+                            item.add(s[1]);
+                        }
+                        else
+                            item.add(dataSet.getFields().get(oldColumnPosition));
+                        item.add(oldColumnValue);
+                        list.add(item);
+                    }
+                }
+                dataSet.setFields(updatedFields);
+                dataSet.setData(list);
+
+                return dataSet;
+
+            } catch (Exception e) {
+                throw new InvalidFormatException("Invalid csv format provided", e);
+            }
         }
 
         return dataSet;
