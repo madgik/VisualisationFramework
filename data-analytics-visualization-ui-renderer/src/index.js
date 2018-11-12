323fff7c28543029cbb94a4c7fb9c8e0735774b9
cmitatakis <cmitatakis@gmail.com>
2018-11-12 13:58:19 +0200

    visualization charts library link first fix

--- data-analytics-visualization-ui-renderer/src/index.js
+++ data-analytics-visualization-ui-renderer/src/index.js
@@ -4,11 +4,13 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import VisualizationRendererInnerContainer from './visualization/containers/VisualizationRendererInnerContainer'
+
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-    ReactDOM.render(<App />, document.getElementById('root'));
-    registerServiceWorker();
-} else{
-    module.exports = {
-        reactComponent: App
-    };
+  ReactDOM.render(<App />, document.getElementById('root'));
+  registerServiceWorker();
+} else {
+  module.exports = { 
+    VisualizationRendererInnerContainer 
+  }
 }
--- data-analytics-visualization-ui-renderer/webpack.config.js
+++ data-analytics-visualization-ui-renderer/webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     libraryTarget: 'commonjs',
     library: '',
-    publicPath: '__data_analytics_visualization_renderer_public_path__'
+    publicPath: '__data_analytics_visualization_ui_renderer_public_path__'
   },
   module: {
     rules: [{
@@ -45,7 +45,7 @@ module.exports = {
 
   plugins: [
     new ReplacePlugin([{
-      partten: /"__data_analytics_visualization_renderer_public_path__"/g,
+      partten: /"__data_analytics_visualization_ui_renderer_public_path__"/g,
       replacement: function () {
         return 'window.staticFileBaseUrl';
       }
