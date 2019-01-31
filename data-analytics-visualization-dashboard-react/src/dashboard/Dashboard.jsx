50ebcd4591dd99ebad07f1bb5f3a1a47e2963fd1
cmitatakis <cmitatakis@gmail.com>
2019-01-31 14:55:44 +0200

    version updated to 1.0.7

--- data-analytics-visualization-dashboard-react/src/dashboard/Dashboard.jsx
+++ data-analytics-visualization-dashboard-react/src/dashboard/Dashboard.jsx
@@ -8,6 +8,8 @@ import MapConfigurationContainer from './containers/MapConfigurationContainer';
 import LoadingBar from 'react-redux-loading-bar'
 import FieldInfoContainer from './containers/FieldInfoContainer';
 import ChartHeaderContainer from './containers/ChartHeaderContainer';
+import 'react-sticky-header/styles.css';
+import StickyHeader from 'react-sticky-header';
 
 class Dashboard extends React.Component {
 
@@ -26,6 +28,27 @@ class Dashboard extends React.Component {
     return (
 
       <div className='App-style'>
+{/* <StickyHeader
+    // This is the sticky part of the header.
+    header={
+      <div className="Header_root">
+        <h1 className="Header_title">ReactStickyHeader</h1>
+ 
+        <ul className="Header_links">
+          <li className="Header_link">When</li>
+          <li className="Header_link">Why</li>
+          <li className="Header_link">About</li>
+        </ul>
+      </div>
+    }
+  >
+    <section>
+      <p>
+        This section will be what the sticky header scrolls over before entering into
+        sticky state. See the gif above or run the test story book to see examples.
+      </p>
+    </section>
+  </StickyHeader> */}
        <header>
         <LoadingBar updateTime={600} maxProgress={90} progressIncrease={5}/>
         <br></br> 
