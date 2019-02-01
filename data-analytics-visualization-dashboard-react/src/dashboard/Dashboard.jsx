6c2efa502b3963f49117daa93f7abd64c4bf886d
cmitatakis <cmitatakis@gmail.com>
2019-02-01 18:14:05 +0200

    loading bar fix

--- data-analytics-visualization-dashboard-react/src/dashboard/Dashboard.jsx
+++ data-analytics-visualization-dashboard-react/src/dashboard/Dashboard.jsx
@@ -8,75 +8,55 @@ import MapConfigurationContainer from './containers/MapConfigurationContainer';
 import LoadingBar from 'react-redux-loading-bar'
 import FieldInfoContainer from './containers/FieldInfoContainer';
 import ChartHeaderContainer from './containers/ChartHeaderContainer';
-import 'react-sticky-header/styles.css';
-import StickyHeader from 'react-sticky-header';
+
 
 class Dashboard extends React.Component {
 
   componentWillMount() {
 
-   
+
   }
   render() {
     var queries = [{
       columns: 2,
       query: 'min-width: 1350px'
     }];
-    
+
     const mmRenderRef = React.createRef();
 
     return (
 
       <div className='App-style'>
-{/* <StickyHeader
-    // This is the sticky part of the header.
-    header={
-      <div className="Header_root">
-        <h1 className="Header_title">ReactStickyHeader</h1>
- 
-        <ul className="Header_links">
-          <li className="Header_link">When</li>
-          <li className="Header_link">Why</li>
-          <li className="Header_link">About</li>
-        </ul>
-      </div>
-    }
-  >
-    <section>
-      <p>
-        This section will be what the sticky header scrolls over before entering into
-        sticky state. See the gif above or run the test story book to see examples.
-      </p>
-    </section>
-  </StickyHeader> */}
-       <header>
-        <LoadingBar updateTime={1200} maxProgress={90} progressIncrease={5}/>
-        <br></br> 
-      </header>
-        <div>
-          <HeaderMenuContainer> </HeaderMenuContainer>
+
+        <div  className="headerMenu">
+        <LoadingBar updateTime={1200} maxProgress={90} progressIncrease={5} showFastActions />
+
         </div>
-        <br></br>
-        <Columns columns={2} gap='8px' queries={queries}
-        >
-          <div className='ui clearing segment'>
-            <VisualizationRendererContainer size={this.props.size} mmRender={mmRenderRef} />
-            <MapConfigurationContainer />
-          </div>
-          <div >
+
+        <div className="content">
+        <HeaderMenuContainer> </HeaderMenuContainer>
+
+          <Columns columns={2} gap='8px' queries={queries}
+          >
             <div className='ui clearing segment'>
-                <FieldInfoContainer></FieldInfoContainer>
+              <VisualizationRendererContainer size={this.props.size} mmRender={mmRenderRef} />
+              <MapConfigurationContainer />
             </div>
-            <div className='ui clearing segment'>
+            <div >
+              <div className='ui clearing segment'>
+                <FieldInfoContainer></FieldInfoContainer>
+              </div>
+              <div className='ui clearing segment'>
                 <ChartHeaderContainer></ChartHeaderContainer>
                 <Chart2RenderContainer size={this.props.chartsSize} mmRender={mmRenderRef} />
-            </div>
-            <div className='ui clearing segment'>
-                <ChartHeaderContainer></ChartHeaderContainer>    
+              </div>
+              <div className='ui clearing segment'>
+                <ChartHeaderContainer></ChartHeaderContainer>
                 <ChartRenderContainer size={this.props.chartsSize} mmRender={mmRenderRef} />
+              </div>
             </div>
-          </div>
-        </Columns>
+          </Columns>
+        </div>
       </div>
 
 
