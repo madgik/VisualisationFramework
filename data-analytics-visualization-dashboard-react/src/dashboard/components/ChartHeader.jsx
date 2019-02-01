c0d31c823eb21e0c0894eeba2d986a1cfd0705a5
cmitatakis <cmitatakis@gmail.com>
2019-02-01 11:06:00 +0200

    field info

--- data-analytics-visualization-dashboard-react/src/dashboard/components/ChartHeader.jsx
+++ data-analytics-visualization-dashboard-react/src/dashboard/components/ChartHeader.jsx
@@ -22,7 +22,7 @@ class ChartHeader extends React.Component {
             }
       ;
     return (
-    <div className='ui clearing '>
+    <div >
         <Header as='h3'>{this.props.title}</Header>
         <Dropdown style={styles} placeholder='Configure'  selection  />
   </div>
