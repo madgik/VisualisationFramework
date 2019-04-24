96fe8af477c56337494be9faea1610318620ba24
cmitatakis <cmitatakis@gmail.com>
2019-04-24 16:19:20 +0300

    production build fix

--- data-analytics-network-graph-ui-react/src/network-graph/actions/controlGraph.actions.jsx
+++ data-analytics-network-graph-ui-react/src/network-graph/actions/controlGraph.actions.jsx
@@ -113,27 +113,38 @@ function addGraphData(newData, graphData) {
 
 function getDateGraph(date, graphData, graphId) {
   return function (dispatch) {
-    var nodeIds = '';
-    // var nodeIds = [];
+   // var nodeIds =  new Array();
+    var nodeIds = new URLSearchParams();
+
+    var nodes = [];
+   // var nodeIds = [];
     graphData.nodes.forEach(element => {
-      // nodeIds.push(element.id)
-       nodeIds +="nodes[]="+element.id +",";
+     // nodes.push(element.id);
+      // nodeIds +=+element.id +",";
+      nodeIds.append("nodes", element.id);
+
     });
 
+    console.log(nodeIds);
     // var params =   
     //  {
     //   nodeIds,
     //   date: date
     // }
-    var params =nodeIds + "date="+date;
-    //  params = Ajax.buildUrlParameters(params);
+
+   // var params =nodeIds + "date="+date;
+    //params = Ajax.buildUrlParameters(params);
+  //  console.log(params);
     //FOR PRODUCTION PORTLET PARAMETERS
     // , JSON.stringify(params)  // , params
-    var resourceUrl = Ajax.buildUrl(Ajax.NETWORK_GRAPH_BASE_PATH + "/" + Ajax.NETWORK_GRAPH_DATE_PATH + "/" + graphId );
-  //  console.log(resourceUrl);
+    nodeIds.append("date", date);
+
+    var resourceUrl = Ajax.buildUrl(Ajax.NETWORK_GRAPH_BASE_PATH + "/" + Ajax.NETWORK_GRAPH_DATE_PATH + "/" + graphId,nodeIds);
+    console.log(resourceUrl);
     // console.log("get string:" + JSON.stringify(nodeIds))
     // ,{params:{nodes: nodeIds,date: date}}
-    return axios.get(resourceUrl,{params:{nodes: nodeIds,date: date}})
+    console.log(nodeIds);
+    return axios.get(resourceUrl)
       .then(response => {
 
         dispatch(addGraphData(response.data, graphData))
--- data-analytics-visualization-service/src/main/java/gr/uoa/di/aginfra/data/analytics/visualization/service/controllers/ConfigurationController.java
+++ data-analytics-visualization-service/src/main/java/gr/uoa/di/aginfra/data/analytics/visualization/service/controllers/ConfigurationController.java
@@ -93,6 +93,7 @@ public class ConfigurationController extends BaseController {
 
 		configurationService.storeConfiguration(entity);
 
+
 		return ResponseEntity.ok("");
 	}
 
