cb199d7dc6fb6205996598dadc67e9d1e14f42d5
vfloros <vfloros@cite.gr>
2020-01-17 11:37:24 +0200

    Modification for the Http requests to trust all hosts

--- data-analytics-visualization-dashboard-react/src/index.js
+++ data-analytics-visualization-dashboard-react/src/index.js
@@ -1,16 +1,8 @@
-/* FOR PORTLET BUILD comment THIS */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
-/* FOR PORTLET BUILD comment above and uncomment bellow */
-// const React=require('react').default;
-// const ReactDOM = require('react-dom').default;
-// require('./index.css');
-// const App = require('./App').default;
-// const registerServiceWorker = require('./registerServiceWorker').default;
 
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
     ReactDOM.render(<App />, document.getElementById('root'));
